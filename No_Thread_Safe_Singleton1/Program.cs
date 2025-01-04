184216e9a3c73e195870d7e270205e12b75f9893
Adinarayana Reddy JC <jc.adi101@gmail.com>
2025-01-04 14:35:09 +0530

    No Thread Safe Singleton

---
+++ No_Thread_Safe_Singleton1/Program.cs
@@ -0,0 +1,36 @@
+﻿
+//Call the GetInstance static method to get the Singleton Instance
+Singleton fromTeachaer = Singleton.Instance;
+fromTeachaer.PrintDetails("From Teacher");
+//Call the GetInstance static method to get the Singleton Instance
+Singleton fromStudent = Singleton.Instance;
+fromStudent.PrintDetails("From Student");
+Singleton fromStudent1 = Singleton.Instance;
+fromStudent1.PrintDetails("From Student1");
+Console.ReadLine();
+
+public sealed class Singleton
+{
+    private static int Counter = 0;
+    private Singleton() {
+        Counter++;
+        Console.WriteLine("Counter Value " + Counter.ToString());
+    }
+    private static Singleton instance = null;
+    public static Singleton Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+    public void PrintDetails(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+}
